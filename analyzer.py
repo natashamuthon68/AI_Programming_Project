59f80d7bb03b18243c623d3c8b2191d425dedcef
Natasha Muthoni <natashaangela0553@gmail.com>
2026-04-14 23:09:44 +0300

    Remove the scikit-learn imports from analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+
 
 # Load the dataset from CSV
 def load_data():
